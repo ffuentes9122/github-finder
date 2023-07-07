cfadb6498f40a78ae9e3f3f1e7b3ccef8ac3a90e
ffuentes9122 <fabian.fuentes9122@gmail.com>
2023-07-07 05:45:10 -0600

    Routes and pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import About from './pages/About';
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
+import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
+
 function App() {
   return (
-    <div>
-      <h1>Hello world</h1>
-    </div>
+    <Router>
+      <div className='flex flex-col justify-between h-screen'>
+        <Navbar />
+        <main className='container mx-auto px-3 pb-12'>
+          <Routes>
+            <Route path='/about' element={<About />}></Route>
+            <Route path='/' element={<Home />}></Route>
+            <Route path='/notfound' element={<NotFound />}></Route>
+            <Route path='/*' element={<NotFound />}></Route>
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
